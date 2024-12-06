a8a3bb5b37dc4d509b8c9a3448c7d5f004edb55d
Farid Satria <faridsatria24@gmail.com>
2024-12-06 15:19:34 +0700

    fixing eror

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Jzm0EHoQ2z9r6lbr2yVDpU/Er8ichhCdn0K7rEwjTGo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
